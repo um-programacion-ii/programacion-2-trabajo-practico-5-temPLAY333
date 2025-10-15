beb07c1b729c23e1d26ea63ba503b323c731e342
temPLAY333 <t.bourguet@alumno.um.edu.ar>
2025-10-15 16:36:03 -0300

    feature/issue-2-init

---
+++ src/test/java/um/prog2/TP5/Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package um.prog2.TP5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
